1f722b45a144f30616183380c1f7b104ca70c9ad
Alexandre Jacquot <Alexandre Jacquot>
2016-03-24 13:12:52 +0100

    test commit depuis windows

--- main.cpp
+++ main.cpp
@@ -5,12 +5,21 @@ using namespace std;
 
 int main()
 {
+    bool yolo = true;
     Window superfenetre{"kikou", sf::Style::Default};
-    while (true)
+    sf::Event event;
+    while (yolo)
     {
+        while (superfenetre.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                superfenetre.close();
+                yolo = false;
+            }
+        }
         superfenetre.clear();
         superfenetre.display();
     }
     return 0;
 }
-
