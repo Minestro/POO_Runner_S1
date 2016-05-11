506d15b62871bb5296465d2f1b289eac5550f631
Alexandre Jacquot <alexandre.jacquot@etu.u-bordeaux.fr>
2016-05-12 00:14:02 +0200

    pointeur dans main

--- main.cpp
+++ main.cpp
@@ -15,39 +15,43 @@ int main()
     Window superfenetre{"Runner", sf::Style::Default, GAME_SIZE_W, GAME_SIZE_H};
     GraphicElement::loadTextures();
     TextElement::loadFonts();
-    Game gameModel{GAME_SIZE_W, GAME_SIZE_H, STARTSPEEDPERIODGAME};
-    GameView gameView{};
-    Menu menuModel{GAME_SIZE_W, GAME_SIZE_H, menuPage::PRE_MENU};
-    MenuView menuView{};
-    gameView.setModel(&gameModel);
-    gameView.setWindow(&superfenetre);
-    menuView.setModel(&menuModel);
-    menuView.setWindow(&superfenetre);
+    Game *gameModel = new Game{GAME_SIZE_W, GAME_SIZE_H, STARTSPEEDPERIODGAME};
+    GameView *gameView = new GameView{};
+    Menu *menuModel = new Menu{GAME_SIZE_W, GAME_SIZE_H, menuPage::PRE_MENU};
+    MenuView *menuView = new MenuView{};
+    gameView->setModel(gameModel);
+    gameView->setWindow(&superfenetre);
+    menuView->setModel(menuModel);
+    menuView->setWindow(&superfenetre);
     while (!quitter)
     {
         sf::sleep(sf::milliseconds(1));
-        if (menuModel.getActivePage() == menuPage::ESCAPE_MENU)
+        if (menuModel->getActivePage() == menuPage::ESCAPE_MENU)
         {
-            gameModel.nextStep();
+            gameModel->nextStep();
             if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-loopTime).count() >= drawPeriod)
             {
-                gameView.synchronise();
-                gameView.draw();
+                gameView->synchronise();
+                gameView->draw();
                 loopTime = std::chrono::system_clock::now();
             }
-            quitter = gameView.treatEvent();
+            quitter = gameView->treatEvent();
         } else {
-            menuModel.refresh();
+            menuModel->refresh();
             if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-loopTime).count() >= drawPeriod)
             {
-                menuView.synchronise();
-                menuView.draw();
+                menuView->synchronise();
+                menuView->draw();
                 loopTime = std::chrono::system_clock::now();
             }
-            quitter = menuView.treatEvent();
+            quitter = menuView->treatEvent();
         }
     }
     GraphicElement::clearTextures();
     TextElement::clearFonts();
+    delete gameModel;
+    delete menuModel;
+    delete gameView;
+    delete menuView;
     return EXIT_SUCCESS;
 }
